0267d632e59d515d20c30e820eabb8e2f43edaab
tearill <1105317666@qq.com>
2020-01-13 16:38:06 +0800

    优化结构，增加数据传递

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -16,7 +16,29 @@ Page({
             console.log(res.data);
         })
     },
+    onPullDownRefresh() {
+        console.log('onPullDownRefresh');
+        wx.showToast({
+            title: '刷新完成',
+            icon: 'success',
+            duration: 2000
+        })
+    },
     onReachBottom() {
-        console.log('触底了') 
+        console.log('触底了');
+    },
+    showDetail(e) {
+        const id = e.currentTarget.dataset.id;
+        console.log(id);
+        // console.log(this.data.goods[0]);
+        wx.navigateTo({
+            url: "../detail/detail?id=" + id,
+            fail: function() {
+                console.log("detail跳转失败");
+            },
+            success: function(res) {
+                console.log("detail跳转成功");
+            }
+        })
     }
 })
