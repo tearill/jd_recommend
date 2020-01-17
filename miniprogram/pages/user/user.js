2c7e380607ddba9a2ca18f73011177769e899e87
tearill <1105317666@qq.com>
2020-01-17 21:19:20 +0800

    添加个人信息界面

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,13 +1,11 @@
 // miniprogram/pages/user/user.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -40,13 +38,6 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
   },
 
