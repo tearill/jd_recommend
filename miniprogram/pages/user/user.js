32b2fa3a4b20968a75a623ad80a00d3ea3e9cbff
tearill <1105317666@qq.com>
2020-01-21 20:28:11 +0800

    重构页面部分结构，添加登陆验证

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,10 +1,59 @@
 // miniprogram/pages/user/user.js
+const loginCacheKey = 'Login:Flag'
 Page({
   data: {
-
+    loginFlag: 0,
+    tag: '登录/注册',
+    bean: 0,
+    options: []
   },
   onLoad(options) {
-
+    wx.request({
+      url: 'http://localhost:1314/userPage',
+      success: (res) => {
+        this.setData({
+          options: res.data.optionData
+        })
+        console.log(res.data.optionData);
+      }
+    })
+    this.setData({
+      loginFlag: wx.getStorageSync(loginCacheKey) || 0
+    })
+    if (this.data.loginFlag === 1) {
+      this.setData({
+        tag: 'Horace'
+      })
+    } else {
+      this.setData({
+        tag: '登录/注册'
+      })
+    };
+  },
+  onShow() {
+    this.onLoad();
+  },
+  login() {
+    if (this.data.loginFlag === 0) {
+      wx.navigateTo({
+        url: '../login/login',
+        success: function () {
+          console.log('login跳转成功');
+        },
+        fail: function () {
+          console.log('login跳转失败');
+        }
+      })
+    }
+  },
+  logout() {
+    wx.setStorage({
+      key: loginCacheKey,
+      data: 0
+    });
+    wx.navigateTo({
+      url: '../login/login'
+    })
   },
   showAbout() {
     wx.navigateTo({
@@ -27,6 +76,41 @@ Page({
         console.log('feedback跳转失败');
       }
     })
+  },
+  showAppointment() {
+    wx.navigateTo({
+      url: '../appointment/appointment',
+      success: function () {
+        console.log('appointment跳转成功');
+      },
+      fail: function () {
+        console.log('appointment跳转失败');
+      }
+    })
+  },
+  showDiscount() {
+    wx.navigateTo({
+      url: '../discount/discount',
+      success: function () {
+        console.log('discount跳转成功');
+      },
+      fail: function () {
+        console.log('discount跳转失败');
+      }
+    })
+  },
+  showOptions(e) {
+    const index = e.currentTarget.dataset.index;
+    console.log(index);
+    if (this.data.loginFlag === 0) {
+      wx.navigateTo({
+        url: '../login/login'
+      })
+    } else {
+      wx.navigateTo({
+        url: '../account/account?index=' + index
+      })
+    }
   }
 
 })
