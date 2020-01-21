273e439851c67b640895ff4105489c61c8d84d0c
tearill <1105317666@qq.com>
2020-01-21 20:25:46 +0800

    添加页面分享事件

--- miniprogram/pages/explore/explore.js
+++ miniprogram/pages/explore/explore.js
@@ -109,5 +109,15 @@ Page({
                 console.log("goodsDetail跳转成功");
             }
         })
+    },
+    // 分享功能
+    onShareAppMessage() {
+        return {
+            title: '什么值得买京东优选',
+            path: 'pages/explore/explore',
+            success: function() {
+                console.log('分享成功')
+            }
+        }
     }
 })
