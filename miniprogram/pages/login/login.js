55e666840c05bc71e18fe7781d30e63707b70bf2
tearill <1105317666@qq.com>
2020-01-23 18:39:16 +0800

    修改跳转逻辑

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -2,41 +2,57 @@
 const loginCacheKey = 'Login:Flag'
 Page({
   data: {
-
+    prev: ''
   },
-  onLoad() {
-
+  onLoad(options) {
+    // let pages = getCurrentPages(); // 当前界面
+    // let prevpage = pages[pages.length - 2]; // 跳转过来的界面
+    console.log(options.prev, '跳转过来的界面');
+    this.setData({
+      prev: options.prev
+    })
   },
   // 获取手机号
   getPhoneNumber(e) {
+    var that = this;
     console.log(e.detail.errMsg);
     console.log(e.detail.iv);
     console.log(e.detail.encryptedData);
+
     wx.login({
       success: function () {
-        wx.setStorage({
-          key: loginCacheKey,
-          data: 1
-        });
-        wx.switchTab({
-          url: '../user/user'
-        })
+        // wx.setStorage({
+        //   key: loginCacheKey,
+        //   data: 1
+        // });
+        // wx.switchTab({ // 跳转tabBar页面
+        //   url: '../user/user'
+        // })
       },
       fail: function () {
+        // wx.setStorage({
+        //   key: loginCacheKey,
+        //   data: 1
+        // });
+        // wx.switchTab({
+        //   url: '../user/user'
+        // })
+      },
+      complete: function () {
         wx.setStorage({
           key: loginCacheKey,
           data: 1
         });
-        wx.switchTab({
-          url: '../user/user'
-        })
+        if (that.data.prev === 'user') {
+          wx.switchTab({
+            url: '../user/user'
+          })
+        } else {
+          wx.navigateTo({
+            url: '../' + that.data.prev + '/' + that.data.prev
+          })
+        }
       }
     })
-
-    // setTimeout(() => {
-    //   wx.switchTab({
-    //     url: '../user/user'
-    //   })
-    // }, 2000);
   }
 })
