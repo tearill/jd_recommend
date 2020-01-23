a020dae6654bdff8e88513f45a1478ed2edaa839
tearill <1105317666@qq.com>
2020-01-23 18:37:28 +0800

    修改跳转逻辑

--- miniprogram/pages/appointment/appointment.js
+++ miniprogram/pages/appointment/appointment.js
@@ -1,66 +1,24 @@
 // miniprogram/pages/appointment/appointment.js
+const loginCacheKey = 'Login:Flag';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    flag: wx.getStorageSync(loginCacheKey) || 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    this.setData({
+      flag: wx.getStorageSync(loginCacheKey)
+    })
+    if (this.data.flag === 0) {
+      wx.navigateTo({
+        url: '../login/login'
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShow() {
+    if (this.data.flag === 0) {
+      wx.switchTab({
+        url: '../user/user'
+      })
+    }
   }
 })
