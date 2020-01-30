2aee4f10ce1d9aae4de9c9789ff9803e546f1f5b
tearill <1105317666@qq.com>
2020-01-30 23:51:45 +0800

    修改全局配置

--- app.js
+++ app.js
@@ -32,8 +32,19 @@ App({
         }
       }
     })
+    //获取设备顶部窗口的高度（不同设备窗口高度不一样，根据这个来设置自定义导航栏的高度）
+    //这个最初我是在组件中获取，但是出现了一个问题，当第一次进入小程序时导航栏会把
+    //页面内容盖住一部分,当打开调试重新进入时就没有问题，这个问题弄得我是莫名其妙
+    //虽然最后解决了，但是花费了不少时间
+    wx.getSystemInfo({
+      success: (res) => {
+        this.globalData.height = res.statusBarHeight
+      }
+    })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    share: false,  // 分享默认为false
+    height: 0,
   }
 })
