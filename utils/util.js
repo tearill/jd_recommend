331ca0fbf2c889561b217d5e48181a68e9f13ae5
tearill <1105317666@qq.com>
2020-02-10 22:40:55 +0800

    添加loading展示，优化体验

--- utils/util.js
+++ utils/util.js
@@ -1,9 +1,21 @@
 const request = myUrl => {
+  wx.showLoading({
+    title: '加载中' //提示的内容
+  });
   return new Promise((resolve, reject) => {
     wx.request({
       url: myUrl,
-      success: res => resolve(res),
-      fail: res => reject(res)
+      success: res => {
+        wx.hideLoading();
+        resolve(res);
+      },
+      fail: res => {
+        wx.hideLoading();
+        reject(res);
+      },
+      complete: () => {
+        wx.hideLoading();
+      }
     })
   })
 }
